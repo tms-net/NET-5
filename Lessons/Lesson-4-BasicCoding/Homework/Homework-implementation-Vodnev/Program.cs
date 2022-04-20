0fb73fc53ec3a2b8d68ceb633f88d4fc3f1e9e5c
Даник <voddden@gmail.com>
2022-04-20 21:33:16 +0300

    Выполнение задания

--- Lessons/Lesson-4-BasicCoding/Homework/Homework-implementation-Vodnev/Program.cs
+++ Lessons/Lesson-4-BasicCoding/Homework/Homework-implementation-Vodnev/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество продуктов: ");
+int prodNumber = int.Parse(Console.ReadLine());
+
+var products = new Product[prodNumber];
+
+string name_input;
+double price_input;
+double weigth_input;
+for (int i = 0; i < products.Length; i++)
+{
+    Console.Write($"Введите имя продукта #{i + 1}: ");
+    name_input = Console.ReadLine();
+
+    Console.Write($"Введите цену продукта #{i + 1}: ");
+    price_input = double.Parse(Console.ReadLine());
+
+    Console.Write($"Введите вес продукта #{i + 1}: ");
+    weigth_input = double.Parse(Console.ReadLine());
+
+    products[i] = new Product(name_input, price_input, weigth_input);
+}
+
+Console.WriteLine("\n================= Вывод информации о продуктах: ================\n");
+
+int номер_продукта = 0;
+foreach (Product product in products)
+{
+    ++номер_продукта;
+    product.Print(номер_продукта);
+}
+
+class Product
+{
+    private string name;
+    private double price;
+    private double weight;
+
+    public Product(string name = "<нет данных>", double price = -1, double weight = -1)
+    {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    
+
+    public void Print(int номер_продукта)
+    {
+        Console.WriteLine($"---Информация о продукте #{номер_продукта}: \nИмя: {name} \nЦена: {price} \nВес: {weight}");
+    }
+}
