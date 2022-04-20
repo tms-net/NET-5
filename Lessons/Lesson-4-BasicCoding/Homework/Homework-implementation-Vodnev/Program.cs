72d7aadf36c3a0ea918122aa6bb1a1ac3aedfcc5
Даник <voddden@gmail.com>
2022-04-20 21:53:37 +0300

    Добавление проверки ввода данных

--- Lessons/Lesson-4-BasicCoding/Homework/Homework-implementation-Vodnev/Program.cs
+++ Lessons/Lesson-4-BasicCoding/Homework/Homework-implementation-Vodnev/Program.cs
@@ -1,23 +1,41 @@
 ﻿Console.Write("Введите количество продуктов: ");
-int prodNumber = int.Parse(Console.ReadLine());
+int prodNumber = 0;
+try
+{
+    prodNumber = int.Parse(Console.ReadLine());
+}
+catch (Exception)
+{
+    Console.WriteLine("Ошибка, введены некорректные данные");
+    System.Environment.Exit(0);
+}
 
 var products = new Product[prodNumber];
 
-string name_input;
-double price_input;
-double weigth_input;
-for (int i = 0; i < products.Length; i++)
+try
 {
-    Console.Write($"Введите имя продукта #{i + 1}: ");
-    name_input = Console.ReadLine();
+    
+    string name_input;
+    double price_input;
+    double weigth_input;
+    for (int i = 0; i < products.Length; i++)
+    {
+        Console.Write($"Введите имя продукта #{i + 1}: ");
+        name_input = Console.ReadLine();
 
-    Console.Write($"Введите цену продукта #{i + 1}: ");
-    price_input = double.Parse(Console.ReadLine());
+        Console.Write($"Введите цену продукта #{i + 1} (в бел.руб.): ");
+        price_input = double.Parse(Console.ReadLine());
 
-    Console.Write($"Введите вес продукта #{i + 1}: ");
-    weigth_input = double.Parse(Console.ReadLine());
+        Console.Write($"Введите вес продукта #{i + 1} (в кг): ");
+        weigth_input = double.Parse(Console.ReadLine());
 
-    products[i] = new Product(name_input, price_input, weigth_input);
+        products[i] = new Product(name_input, price_input, weigth_input);
+    }
+}
+catch (Exception)
+{
+    Console.WriteLine("Ошибка, введены некорректные данные");
+    System.Environment.Exit(0);
 }
 
 Console.WriteLine("\n================= Вывод информации о продуктах: ================\n");
@@ -42,10 +60,8 @@ class Product
         this.weight = weight;
     }
 
-    
-
     public void Print(int номер_продукта)
     {
-        Console.WriteLine($"---Информация о продукте #{номер_продукта}: \nИмя: {name} \nЦена: {price} \nВес: {weight}");
+        Console.WriteLine($"---Информация о продукте #{номер_продукта}: \nИмя: {name} \nЦена: {price} бел.руб. \nВес: {weight} кг");
     }
 }
