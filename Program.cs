8056e30578aab41ae953c05cc77c49ed47b6cb5f
danetolk <94922898+deniman23@users.noreply.github.com>
2022-04-21 14:08:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using System.Collections.Generic;
+
+
+Console.WriteLine("Домашнее задание. Создать список продуктов.");
+
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Введите количество продуктов:");
+var prodNumber = int.Parse(Console.ReadLine());
+List<int> price = new List<int>();
+List<int> height = new List<int>();
+for (int i = 0; i < prodNumber; i++)
+{
+    Console.WriteLine("Введите цену продукта:");
+    price.Add(Convert.ToInt32(Console.ReadLine()));
+    Console.WriteLine("Введите количество продукта:");
+    height.Add(Convert.ToInt32(Console.ReadLine()));
+
+}
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Список цен");
+
+foreach (var Prices in price)
+{
+    Console.WriteLine(Prices);
+    
+}
+
+Console.WriteLine("Средняя сумма:");
+
+var average = price.Average();
+Console.WriteLine(average);
+
+
+
+public class Product
+{
+    //void ShowProd();
+}
+
+
