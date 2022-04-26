358eb17d352d1534d030d3cf1e22cc740364dcfb
YkubikD <y_kubik@icloud.com>
2022-04-26 23:44:40 +0300

    Test

---
+++ Lessons/Lesson-5-BasicCoding/Homework/Home5/Home/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vuvod
+{
+    public class Vuvuda
+    {
+        public string pron;
+        public string proc;
+        public string proy;
+        public void MMM ()  
+        {
+            Console.WriteLine(pron);
+            Console.WriteLine(proc);
+            Console.WriteLine(proy);
+        }
+    }
+
+}
---
+++ Lessons/Lesson-5-BasicCoding/Homework/Home5/Home/Program.cs
@@ -0,0 +1,50 @@
+﻿using Vuvod;
+
+
+Vuvuda [] Products = new Vuvuda[15];
+var namB = 0;
+var namC = 0;
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine("Введите название товара или для завершения введите 999");
+    string proN = Console.ReadLine();
+    if ( proN == "999")
+    {
+        break;
+    }
+    Console.WriteLine("Введите цвет товара");
+    string proC = Console.ReadLine();
+    Console.WriteLine("Введите отличительную особбеность ");
+    string proY = Console.ReadLine();
+    var prod = new Vuvuda { pron = proN, proc = proC, proy = proY};
+    Products [i]= prod;
+    namB=i; // количество записей
+    namC = namB + 1; // количество итог запись
+    Console.WriteLine("Всего позиций " +  namC);
+}
+while (true)
+{
+    Console.WriteLine("Введите позицию для просмотра");
+    var v = Console.ReadLine();
+    int b;
+    if (int.TryParse(v, out b))
+    {
+        Products[b - 1].MMM();
+
+    }
+    else 
+    {
+        break;
+    }
+    
+}
+for (int i = 0; i <namB ; i++)
+    {
+        Products[i].MMM();
+        
+    }
+
+
+
+
+
