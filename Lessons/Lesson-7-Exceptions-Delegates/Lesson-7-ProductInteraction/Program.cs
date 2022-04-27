d0fa3708619f2a84ff26b73f8f72cd88653ac0c7
Dzmitry Andryienka <dandryienka@microsoft.com>
2022-04-27 11:38:20 +0300

    Homewok lesson 7

---
+++ Lessons/Lesson-7-Exceptions-Delegates/Lesson-7-ProductInteraction/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, %Username%! Put your card into ATM!");
+
+string cardNumber = null;
+
+// Создание объекта, симулирующего действия пользователя
+var client = new ATMClient();
+
+client.DoRandomActions();
+
+Console.WriteLine($"Goodbye, you card {cardNumber} was released!");
+
+
+
+
