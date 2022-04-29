2b76ff834d07e97cb7ed26ffb7bd761d38d6ee01
Dmitry <diman.84@gmail.com>
2022-04-29 12:52:05 +0300

    Lesson 8 implementation

---
+++ Lessons/Lesson-7-Exceptions-Delegates/Lesson-7-ATM/ATMAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class ATMAccount
+{
+    private const int MaxAmount = 100;
+
+    // Should have history of operations
+
+    public string CardNumber { get; }
+    public int Ballance { get; private set; }
+
+    public ATMAccount(string cardNumber)
+    {     
+        CardNumber = cardNumber;
+        Ballance = 100;
+    }
+
+    public void WithdrawMoney(int amount)
+    {
+        if (Ballance < amount)
+        {
+            throw new InvalidOperationException("Недостаточно средств");
+        }
+
+        Ballance -= amount;
+    }
+
+    public void AddMoney(int amount)
+    {
+        if (amount > MaxAmount)
+        {
+            throw new InvalidOperationException("Превышен лимит пополнения");
+        }
+
+        Ballance += amount;
+    }
+}
