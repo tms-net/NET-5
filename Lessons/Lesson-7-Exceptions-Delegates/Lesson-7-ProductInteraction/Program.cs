e4137ec1b3097950665614fc378f1167efe92906
Dmitry <diman.84@gmail.com>
2022-04-29 12:52:05 +0300

    Lesson 8 implementation

--- Lessons/Lesson-7-Exceptions-Delegates/Lesson-7-ProductInteraction/Program.cs
+++ Lessons/Lesson-7-Exceptions-Delegates/Lesson-7-ProductInteraction/Program.cs
@@ -1,15 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
+using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
 Console.WriteLine("Hello, %Username%! Put your card into ATM!");
 
-string cardNumber = null;
+string myCardNumber = null;
 
 // Создание объекта, симулирующего действия пользователя
 var client = new ATMClient();
 
-client.DoRandomActions();
+client.InvalidOperation += message => Console.WriteLine($"Ошибка проведения операции: {message}");
+client.ModifiedAccount += ballance => Console.WriteLine($"Ваша новый балланс: {ballance}");
+client.ViewingAccount += ballance => Console.WriteLine($"Ваша балланс: {ballance}");
 
-Console.WriteLine($"Goodbye, you card {cardNumber} was released!");
+client.ModifyingAccount += args =>
+{
+    Console.WriteLine($"Введите кол-во денег для карточки {args.CardNumber}:");
 
+    args.Amount = int.Parse(Console.ReadLine());
+};
 
+client.CardInserted += cardNumber =>
+{
+    myCardNumber = cardNumber;
+    Console.WriteLine($"{cardNumber}.");
+};
 
+try
+{
+    client.DoRandomActions();
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Непредвиденная ошибка: {ex.Message}");
+}
 
+Console.WriteLine($"Goodbye, you card {myCardNumber} was released!");
