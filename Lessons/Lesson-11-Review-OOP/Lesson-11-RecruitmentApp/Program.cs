3a2e42ce54c2b87b88bfc95954dc326f24df66b9
Dmitry <diman.84@gmail.com>
2022-05-13 18:14:33 +0300

    Lesson 11 Homework

---
+++ Lessons/Lesson-11-Review-OOP/Lesson-11-RecruitmentApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Lesson_11_RecruitmentApp;
+
+Console.WriteLine("Отдел кадров приветствует вас!");
+
+String RecordType = "Vacancy";
+
+Record softwareEngineerVacancy = new Record("SoftwareEngineer0001", RecordType);
+Record frontEndEngineerVacancy = new Record("FrontEndEngineer0020", RecordType);
+
+/**
+* TODO: Разработать сервис и вызвать действия так как указано:
+*/
+
+// Вызвать действие "Submit For Approval" для softwareEngineerVacancy
+// Вызвать действие "Approve" для softwareEngineerVacancy
+
+// Вызвать действие "Submit For Approval" для frontEndEngineerVacancy
+// Вызвать действие "Reject" для frontEndEngineerVacancy
+
+/**
+    //TODO: Разработать сервис
+
+    public class <Service> {
+
+    }
+
+*/
