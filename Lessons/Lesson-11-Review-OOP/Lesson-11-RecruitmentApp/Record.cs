268a67d5f420e501e39d3f91a2f359c73f4a3a15
Dmitry <diman.84@gmail.com>
2022-05-13 18:14:33 +0300

    Lesson 11 Homework

---
+++ Lessons/Lesson-11-Review-OOP/Lesson-11-RecruitmentApp/Record.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11_RecruitmentApp
+{
+    public class Record
+    {
+        public string Id { get; private set; }
+        public string Type { get; private set; }
+
+        public Record(string id, string type)
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+}
