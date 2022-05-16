509b57fefd0f9cc0f61ca1e9216b249387cf2a0b
Alex-Ecstasy <alex29ecstasy@gmail.com>
2022-05-16 22:47:54 +0300

    ДЭЗЭ11

---
+++ Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/CoffeeMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11_CofeeMachine
+{
+    public class CoffeeMachine
+    {
+        public string name;
+        public int gramsOfCoffee = 0;
+        public int gramsOfWater = 0;
+        public int gramsOfMilk = 0;
+        public int MakeDrink()
+        {
+            int millilitersOfDrink = gramsOfWater + gramsOfMilk;
+            return millilitersOfDrink;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Ваш выбор: {0}",name);
+            Console.WriteLine("Использовано кофе: {0} грамм", gramsOfCoffee);
+            Console.WriteLine("Количество воды: {0} грамм", gramsOfWater);
+            Console.WriteLine("Количество молока: {0} грамм", gramsOfMilk);
+            Console.WriteLine("Готовый напиток: {0} мл", MakeDrink());
+            Console.WriteLine("Кушай, не обляпайся");
+        }
+        public enum drink
+        {
+            Espresso,
+            Americano,
+            Cappuchino
+        }
+        public IEnumerable<string> GetAvailableDrinks()
+        {
+
+            return Enumerable.Empty<string>();
+        }
+    }
+    
+}
--- Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/Program.cs
+++ Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/Program.cs
@@ -4,16 +4,61 @@ using System;
 
 Console.WriteLine("Hello, %Username%!");
 
-var cofeeMachine = new CofeeMachine();
+//var coffeeMachine = new CoffeeMachine();
 
 Console.WriteLine("Выберите напиток:");
+Console.WriteLine("1 - Espresso");
+Console.WriteLine("2 - Americano");
+Console.WriteLine("3 - Cappuccino");
+//foreach (var drinks in CoffeeMachine.drink)
+//{
+//    Console.WriteLine(drinks);
+//}
+//foreach (var drink in coffeeMachine.GetAvailableDrinks()) 
+//{
+//    Console.WriteLine(drink);
+//}
+string userDrink = Console.ReadLine();
 
-foreach (var drink in cofeeMachine.GetAvailableDrinks()) 
-{
-    Console.WriteLine(drink);
-}
-
-// TODO: Обработать выбор пользователя и использовать объект CofeeMachine
+// TODO: Обработать выбор пользователя и использовать объект CoffeeMachine
 // для приготовления напитка, вывести информацию о приготовленном напитке.
 
 
+CoffeeMachine espresso = new CoffeeMachine();
+{
+    espresso.name = "Espresso";
+    espresso.gramsOfCoffee = 8;
+    espresso.gramsOfWater = 30;
+    espresso.gramsOfMilk = 0;
+}
+
+CoffeeMachine americano = new CoffeeMachine();
+{
+    americano.name = "Americano";
+    americano.gramsOfCoffee = 8;
+    americano.gramsOfWater = 150;
+    americano.gramsOfMilk = 0;
+}
+CoffeeMachine cappuccino = new CoffeeMachine();
+{
+    cappuccino.name = "Cappuccino";
+    cappuccino.gramsOfCoffee = 10;
+    cappuccino.gramsOfWater = 30;
+    cappuccino.gramsOfMilk = 200;
+}
+
+switch (userDrink)
+{
+    case "1":
+        espresso.GetInfo();
+        break;
+    case "2":
+        americano.GetInfo();
+        break;
+    case "3":
+        cappuccino.GetInfo();
+        break;
+    default:
+        Console.WriteLine("Нет такого напитка");
+        break;
+}
