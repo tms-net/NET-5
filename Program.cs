ffcff3af188dd6d9eaaa958348dabe117e48f009
danetolk <94922898+deniman23@users.noreply.github.com>
2022-05-17 14:11:50 +0300

    RecruitmentApp

--- Program.cs
+++ Program.cs
@@ -1,140 +1,119 @@
-﻿using System;
+﻿using Lesson_11_RecruitmentApp;
+using System;
 
-namespace s
+Console.WriteLine("Отдел кадров приветствует вас!");
+
+String RecordType = "Vacancy";
+
+Record softwareEngineerVacancy = new Record("SoftwareEngineer0001", RecordType);
+Record frontEndEngineerVacancy = new Record("FrontEndEngineer0020", RecordType);
+
+/**
+* TODO: Разработать сервис и вызвать действия так как указано:
+*/
+
+// Вызвать действие "Submit For Approval" для softwareEngineerVacancy
+// Вызвать действие "Approve" для softwareEngineerVacancy
+
+// Вызвать действие "Submit For Approval" для frontEndEngineerVacancy
+// Вызвать действие "Reject" для frontEndEngineerVacancy
+
+/**
+    //TODO: Разработать сервис
+
+    public class <Service> {
+
+    }
+*/
+Service service = new Service();
+
+Console.WriteLine("1. " + softwareEngineerVacancy.Id + "\n" +"2. " + frontEndEngineerVacancy.Id);
+
+int number_of_cv = Convert.ToInt32(Console.ReadLine());
+
+string curent_cv = "";
+
+if (number_of_cv == 1)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Name name = new Name();
-
-            Console.WriteLine("Введите name:");
-            name.name_value = Console.ReadLine();
-
-            Price price = new Price();
-
-            Console.WriteLine("Введите price:");
-            price.price_value = int.Parse(Console.ReadLine());
-
-            Count count = new Count();
-
-            Console.WriteLine("Введите count:");
-            count.count_value = int.Parse(Console.ReadLine());
-
-            void ShowProduct()
-            {
-                Console.WriteLine($"Имя: {name.name_value}  цена: {price.price_value}  Кол-во: {count.count_value}");
-            }
-            
-            ShowProduct();
-
-
-            ReklamaInfo obj = new ReklamaInfo(reklama: "Здесь могла быть ваша реклама");
-            obj.WriteReklamaInfo();
-            }
-
-            
-            foreach(var item in list)
-            {
-                Console.WriteLine(String.Format("Имя: {0}, Цена: {1}", item.name, item.price));
-                
-            }
-    interface IReklama
-        {
-            void WriteReklama();
-        }
-
-
-    class ReklamaInfo : IReklama
-        {
-        string reklama;
-
-        public ReklamaInfo(string reklama)
-            {
-            this.reklama = reklama;
-            }
-
-        void IReklama.WriteReklama()
-            {
-                Console.WriteLine(reklama);
-        }
-        
-        public void WriteReklamaInfo()
-            {
-            ReklamaInfo obj = new ReklamaInfo(reklama);
-
-            IReklama link1 = (IReklama)obj;
-            link1.WriteReklama();
-             }
-    }
-        
-
-
-
-
-
-    public class Price
-    {
-        public int price;
-        public int price_value
-        {
-            get { return price; }
-            set
-            {
-                if (value > 0)
-                {
-
-                    price = value;
-                    Console.WriteLine("Значение записано");
-                }
-                else
-                {
-                    throw new Exception("Цена должна быть больше нуля");
-                }
-            }
-        }
+    curent_cv += softwareEngineerVacancy.Id;
+    string service_submit = service.Submit + curent_cv;
+    Console.WriteLine(service_submit);
 }
-   
-    public class Name
-    {
-        public string name;
-        public string name_value
-        {
-            get { return name; }
-            set
-            {
-                if (value != "")
-                {
-                    name = value;
-                    Console.WriteLine("Значение записано");
 
-                }
-                else
-                {
-                    throw new Exception("Введите имя");
-                }
-            }
+else if (number_of_cv == 2)
+{
+    curent_cv += frontEndEngineerVacancy.Id;
+    string service_submit = service.Submit + curent_cv;
+    Console.WriteLine(service_submit);
+}
+
+else
+{
+    throw new Exception();
+}
+
+
+
+Console.WriteLine("1. Approve \n2. Reject.");
+
+int number_of_operation = Convert.ToInt32(Console.ReadLine());
+if(number_of_operation == 1)
+{
+    string service_approve = service.Approve + curent_cv;
+    Console.WriteLine(service_approve);
+}
+else if (number_of_operation == 2)
+{
+    string service_reject = service.Reject + curent_cv;
+    Console.WriteLine(service_reject);
+}
+else
+{
+    throw new Exception();
+}
+
+
+public class Service
+{
+    private string submit_text = "Отправлено на рассмотрение: ";
+
+    public string Submit
+    {
+        get
+        {
+            return submit_text;
+        }
+        set
+        {
+            submit_text = value;
         }
     }
-    public class Count
-    {
-        public int count;
-        public int count_value
-        {
-            get { return count; }
-            set
-            {
-                if (value >= 0)
-                {
-                    count = value;
-                    Console.WriteLine("Значение записано");
 
-                }
-                else
-                {
-                    throw new Exception("Введите количество");
-                }
-            }
+    private string approve_text = "Утверждено: ";
+
+    public string Approve
+    {
+        get
+        {
+            return approve_text;
+        }
+        set
+        {
+            approve_text = value;
+        }
+    }
+
+    private string reject_text = "Отказ: ";
+
+    public string Reject
+    {
+        get
+        {
+            return reject_text;
+        }
+        set
+        {
+            reject_text = value;
         }
     }
 }
