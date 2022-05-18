05c11a395851ed3a970b441a96ad9a2609a4c11e
Dmitry <diman.84@gmail.com>
2022-05-18 10:31:42 +0300

    Update CoffeeMachine implementation

--- Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/CofeeMachine.cs
+++ Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/CofeeMachine.cs
@@ -14,13 +14,112 @@ namespace Lesson_11_CofeeMachine
     /// </summary>
     public class CofeeMachine
     {
-        /// <summary>
-        /// Предоставляет информацию о доступных напитках для приготовления.
-        /// </summary>
-        /// <returns>Названия доступных напитков.</returns>
+        private Drink[] drinks;
+
+        private int totalGrammsCoffee;
+        private int totalGrammsWater;
+        private int totalGrammsMilk;
+
+        public CoffeeMachine(Drink[] drinks)
+        {
+            this.drinks = drinks;
+        }
+
+        public void GetInfo()
+        {
+            //Console.WriteLine("Ваш выбор: {0}", Name);
+            //Console.WriteLine("Использовано кофе: {0} грамм", gramsOfCoffee);
+            //Console.WriteLine("Количество воды: {0} грамм", gramsOfWater);
+            //Console.WriteLine("Количество молока: {0} грамм", gramsOfMilk);
+           // Console.WriteLine("Готовый напиток: {0} мл", MakeDrink());
+            Console.WriteLine("Кушай, не обляпайся");
+        }
+
+        public void PrepareDrink(string userDrink)
+        {
+            IEnumerable<decimal> prices = null;
+            int i = 0;
+            for (; i < 10; i++)
+            {
+                prices = this.drinks.Select(drink => drink.price += i);
+            }
+
+            i = 20;
+
+            var arr = prices.ToArray();
+
+            var drink = this.drinks.First(
+                (drink) =>
+                { 
+                    return drink.Name == userDrink;
+                });
+
+            MakeDrink(drink);
+        }
+
         public IEnumerable<string> GetAvailableDrinks()
         {
-            return Enumerable.Empty<string>();
+
+            return this.drinks.Select(drink => drink.Name);
         }
+
+        private void MakeDrink(Drink drink)
+        {
+            // check coffee
+            if (this.totalGrammsCoffee < drink.gramsOfCoffee)
+            {
+                throw new InvalidOperationException("Не достаточно кофе.");
+            }
+
+            // check water
+            if (this.totalGrammsWater < drink.gramsOfWater)
+            {
+                throw new InvalidOperationException("Не достаточно воды.");
+            }
+
+            // check milk
+            if (this.totalGrammsMilk < drink.gramsOfMilk)
+            {
+                throw new InvalidOperationException("Не достаточно молока.");
+            }
+
+            this.totalGrammsCoffee -= drink.gramsOfCoffee;
+            this.totalGrammsWater -= drink.gramsOfWater;
+            this.totalGrammsMilk -= drink.gramsOfMilk;
+
+            Console.WriteLine($"Prepared {drink.Name}");
+
+            //var output = "Prepared " + drink.name + drink.price.ToString("N"); // Prepared espresso
+
+            //output = string.Format("Prepared {0} {1:N}", drink.name, drink.price);
+
+            //var sb = new StringBuilder();
+            //sb.Append("Prepared ");
+            //sb.AppendFormat("{0}", drink.name);
+            //output = sb.ToString();
+
+            //output = $"Prepared {drink.name} {drink.price:N}";
+
+            Console.WriteLine($"Prepared {drink.Name} {drink.price:N}");
+
+            // ['s1', 's2', ........] 1000
+
+            // var output = ""; // s1,s2,s3
+
+            // string.Join(",", strings);
+        }
+
+        public void AddWater(int amount) => this.totalGrammsWater += amount;
+
+        public void AddCoffee(int amount) => this.totalGrammsCoffee += amount;
+
+        public void AddMilk(int amount) => this.totalGrammsMilk += amount;
+    }
+
+    public enum DrinkType
+    {
+        Espresso,
+        Americano,
+        Cappuchino
     }
 }
--- Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/Program.cs
+++ Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/Program.cs
@@ -4,16 +4,69 @@ using System;
 
 Console.WriteLine("Hello, %Username%!");
 
-var cofeeMachine = new CofeeMachine();
+var espresso = new Drink("Espresso");
+espresso.gramsOfCoffee = 8;
+espresso.gramsOfWater = 30;
+espresso.gramsOfMilk = 0;
+
+var americano = new Drink("Americano")
+{
+    gramsOfCoffee = 8,
+    gramsOfWater = 150,
+};
+
+var cappuccino = new Drink("Cappuccino");
+cappuccino.gramsOfCoffee = 10;
+cappuccino.gramsOfWater = 30;
+cappuccino.gramsOfMilk = 200;
+
+var drinks = new Drink[]
+{
+    espresso, americano, cappuccino
+};
+
+var cofeeMachine = new CoffeeMachine(drinks);
+
+cofeeMachine.AddWater(100);
+cofeeMachine.AddCoffee(500);
+cofeeMachine.AddMilk(300);
+
+//var coffeeMachine = new CoffeeMachine();
 
 Console.WriteLine("Выберите напиток:");
 
-foreach (var drink in cofeeMachine.GetAvailableDrinks()) 
+//Console.WriteLine("1 - Espresso");
+//Console.WriteLine("2 - Americano");
+//Console.WriteLine("3 - Cappuccino");
+
+//foreach (var drinks in CoffeeMachine.drink)
+//{
+//    Console.WriteLine(drinks);
+//}
+//foreach (var drink in coffeeMachine.GetAvailableDrinks()) 
+//{
+//    Console.WriteLine(drink);
+//}
+
+
+while (true)
 {
-    Console.WriteLine(drink);
+    var index = 0;
+    foreach (var drink in cofeeMachine.GetAvailableDrinks())
+    {
+        Console.WriteLine(++index + " - " + drink);
+        // 1 - espresso
+        // 2 - americano   
+        // 3 - cappuccino   
+    }
+
+    string userDrink = Console.ReadLine();
+
+    if (userDrink == "exit")
+    {
+        break;
+    }
+
+    cofeeMachine.PrepareDrink(userDrink);
 }
-
-// TODO: Обработать выбор пользователя и использовать объект CofeeMachine
-// для приготовления напитка, вывести информацию о приготовленном напитке.
-
 
