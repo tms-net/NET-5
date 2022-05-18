f79e04765737d64e2b7ec34718152d2669736036
Dmitry <diman.84@gmail.com>
2022-05-18 10:31:42 +0300

    Update CoffeeMachine implementation

---
+++ Lessons/Lesson-11-Review-OOP/Lesson-11-CofeeMachine/Drink.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11_CofeeMachine
+{
+    public class Drink
+    {
+        public Drink(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public int gramsOfCoffee = 0;
+        public int gramsOfWater = 0;
+        public int gramsOfMilk = 0;
+
+        public decimal price;
+
+        // 12.40
+    }
+}
