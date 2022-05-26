5415a1db7ceaafc6a8b0c585228ca7c2238e7a5a
Dmitry <diman.84@gmail.com>
2022-05-26 18:41:47 +0300

    Initial homework 14

---
+++ Homeworks/HomeWork14/TMS.ShopSimulator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TMS.ShopSimulator
+{
+    class Program
+    {
+	    static int AvailableThreads
+	    {
+		    get
+		    {
+			    ThreadPool.GetAvailableThreads(out int availableThreads, out _);
+			    return availableThreads;
+		    }
+	    }
+
+	    static int MaxThreads
+	    {
+		    get
+		    {
+			    ThreadPool.GetMaxThreads(out int maxThreads, out _);
+			    return maxThreads;
+		    }
+	    }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
