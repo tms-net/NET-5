1cf998922ab4ca93b94d9cf1cf34be8744723c9d
Alex-Ecstasy <alex29ecstasy@gmail.com>
2022-08-03 20:58:03 +0300

    Homework 21 from VSCode

---
+++ Homeworks/HomeWork21/myjs.js
@@ -0,0 +1,17 @@
+function Product(type, price) {
+    this.type = type,
+    this.price = price,
+    this.getInfo = function() {
+        return "Name: " + this.type + ", Price: " + this.price;
+    }
+}
+
+    var p1 = new Product("Milk", "10");
+    var p2 = new Product ("Bread", "8");
+    var p3 = new Product ("HotDog", "12");
+    var p4 = new Product ("HotCat", "15");
+
+    console.log(p1.getInfo());
+    console.log(p2.getInfo());
+    console.log(p3.getInfo());
+    console.log(p4.getInfo());
