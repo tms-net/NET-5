736f9a51614f3edd546f4cac0d1dfff22f97916f
YSeN1997 <ysen1997@gmail.com>
2022-08-08 23:04:27 +0300

    Homework20

--- Homeworks/Homework20/resume.js
+++ Homeworks/Homework20/resume.js
@@ -1,3 +1,30 @@
 // Используйте объект document для поиска и создания элементов DOM
 // Используйте объекты Element для создания дерева DOM
 // Используйте свойство style объектов Element для изменения стилей
+
+
+
+
+var myResume = document.querySelector('h1');myResume.textContent = 'Резюме';
+
+var myName = document.querySelector('myName');myName.textContent = 'Усенко Евгений Игоревич';
+
+
+var myEmail = document.querySelector('myEmail');myEmail.textContent = 'ysen1997@gmail.com';
+
+var myWork = document.querySelector('work');myWork.textContent = 'Работа:';
+
+var myEducation = document.querySelector('education');myEducation.textContent = 'Образование:';
+
+var myAboutMe = document.querySelector('aboutMe');myAboutMe.textContent ='О себе';
+
+var colorArray = ["#24305E", "#116466", "#4F4A41", "#9D8D8F", "#27263D"]; 
+var i = 0; 
+
+function changeColor(){
+    document.body.style.background = colorArray[i]; 
+    i++;
+    if( i > colorArray.length - 1){
+        i = 0;
+    }
+}
