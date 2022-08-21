8393f92b47a412d60e62623d20dc59e8b41c7aa5
Alex-Ecstasy <alex29ecstasy@gmail.com>
2022-08-21 13:20:18 +0300

    Заюзал UseDefaultFiles и переместил все файлы. Странно, но ничего не сломалось

--- Homeworks/Homework21/TMS.NET15.FileServer/Program.cs
+++ Homeworks/Homework21/TMS.NET15.FileServer/Program.cs
@@ -2,9 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.UseStaticFiles(); //я его сюда не ставил, но без него не работает
+app.UseDefaultFiles(new DefaultFilesOptions { DefaultFileNames = new[] { "Html/ajax.html" } });
 
-app.MapGet("/", async context =>
+app.UseStaticFiles();
+
+
+/*app.MapGet("/", async context =>
 {
     var fileContent = File.ReadAllText("wwwroot/ajax.html");
     //var fileContent = File.ReadAllText("wwwroot\ajax.html");
@@ -12,7 +15,7 @@ app.MapGet("/", async context =>
     context.Response.Headers.ContentType = "text/html"; // mime type
     context.Response.StatusCode = 200;
     await context.Response.WriteAsync(fileContent);
-});
+});*/
 app.MapGet("/Text/file", (string file) => File.ReadAllText($"Text/{file}")); // работает без этого, можно отключить
 
 app.Run();
