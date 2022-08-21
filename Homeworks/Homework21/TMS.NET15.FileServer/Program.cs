1c4b9901d2b84d89a64a07fc1473a837853ecdf7
Alex-Ecstasy <alex29ecstasy@gmail.com>
2022-08-21 11:16:02 +0300

    Запустил ДЗ-21

--- Homeworks/Homework21/TMS.NET15.FileServer/Program.cs
+++ Homeworks/Homework21/TMS.NET15.FileServer/Program.cs
@@ -1,10 +1,18 @@
-
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 
+app.MapGet("/", async context =>
+{
+    var fileContent = File.ReadAllText("wwwroot/ajax.html");
+    //var fileContent = File.ReadAllText("wwwroot\ajax.html");
+
+    context.Response.Headers.ContentType = "text/html"; // mime type
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync(fileContent);
+});
+app.MapGet("/content/file", (string file) => File.ReadAllText($"content/{file}"));
+
 app.Run();
-
-
