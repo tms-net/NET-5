1e2b4d973f08cc1e3122bd3677fce99df9e87671
Alex-Ecstasy <alex29ecstasy@gmail.com>
2022-08-23 18:57:39 +0300

    HW22

--- Homeworks/Homework22/TMS.NET15.FileServer/Program.cs
+++ Homeworks/Homework22/TMS.NET15.FileServer/Program.cs
@@ -1,11 +1,47 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+
 var app = builder.Build();
+
+
+app.Use(async (context, next) =>
+{
+    var logger = context.RequestServices.GetService<ILogger>();
+
+    logger.LogInformation($"Request:{context.Request.Path}");
+
+    await next();
+});
 
 app.UseDefaultFiles();
 
 app.UseStaticFiles();
+
+app.MapGet("/config", async context =>
+{
+    IConfiguration config = context.RequestServices.GetService<IConfiguration>();
+
+    //var admin = context.RequestServices.GetService<IOptions<Person>>().Value;
+    string nameEnviroment = app.Environment.EnvironmentName;
+
+    await context.Response.WriteAsync(nameEnviroment);
+
+    await context.Response.WriteAsync(config["NameEnviroment"]);
+
+    //var section = config.GetSection("Admin:Name");
+
+    //await context.Response.WriteAsync(config[app.Environment.EnvironmentName]);
+});
+
+app.MapGet("/page2", async context =>
+    {
+        await context.Response.WriteAsync("You watch page #2");
+});
 
 app.Run();
 
