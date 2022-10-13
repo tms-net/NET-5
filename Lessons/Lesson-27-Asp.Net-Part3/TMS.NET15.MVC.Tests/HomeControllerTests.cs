86f93e294af6642137e9d86b931847846e84a354
Dmitry <diman.84@gmail.com>
2022-10-13 21:48:31 +0300

    Added tests for MVC

---
+++ Lessons/Lesson-27-Asp.Net-Part3/TMS.NET15.MVC.Tests/HomeControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TMS.NET15.MVC.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void SampleTest()
+        {
+        }
+    }
+}
